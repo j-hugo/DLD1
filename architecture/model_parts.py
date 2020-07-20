f7fb4f5c3be4d671508b94d00839aa46a138358a
j-hugo <julian.hugo@student.hpi.de>
2020-07-20 13:12:55 +0200

    folder to organise model architecture

---
+++ architecture/model_parts.py
@@ -0,0 +1,14 @@
+# Model parts for U-Net and ResNet
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True)
+    )
