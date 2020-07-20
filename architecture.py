306e66603ba863c8f78473b01ecedb95eba181c0
seulchan <seulchan07@gmail.com>
2020-07-20 14:07:49 +0200

    update unet

--- architecture.py
+++ architecture.py
@@ -6,14 +6,22 @@ from torchsummary import summary
 def double_conv(in_channels, out_channels):
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.BatchNorm2d(out_channels),
         nn.ReLU(inplace=True),
         nn.Conv2d(out_channels, out_channels, 3, padding=1),
+        nn.BatchNorm2d(out_channels),
         nn.ReLU(inplace=True)
-    )   
+    )
 
+def double_conv_up(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True),
+    )
 
 class UNet(nn.Module):
-
     def __init__(self, n_channel, n_class):
         super().__init__()
                 
@@ -21,13 +29,15 @@ class UNet(nn.Module):
         self.dconv_down2 = double_conv(64, 128)
         self.dconv_down3 = double_conv(128, 256)
         self.dconv_down4 = double_conv(256, 512)        
+        self.dconv_down5 = double_conv(512, 1024)
 
         self.maxpool = nn.MaxPool2d(2)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
         
-        self.dconv_up3 = double_conv(256 + 512, 256)
-        self.dconv_up2 = double_conv(128 + 256, 128)
-        self.dconv_up1 = double_conv(128 + 64, 64)
+        self.dconv_up4 = double_conv_up(512 + 1024, 512)
+        self.dconv_up3 = double_conv_up(256 + 512, 256)
+        self.dconv_up2 = double_conv_up(128 + 256, 128)
+        self.dconv_up1 = double_conv_up(128 + 64, 64)
         
         self.conv_last = nn.Conv2d(64, n_class, 1)
         
@@ -42,19 +52,23 @@ class UNet(nn.Module):
         conv3 = self.dconv_down3(x)
         x = self.maxpool(conv3)   
         
-        x = self.dconv_down4(x)
-        
-        x = self.upsample(x)        
-        x = torch.cat([x, conv3], dim=1)
-        
-        x = self.dconv_up3(x)
-        x = self.upsample(x)        
-        x = torch.cat([x, conv2], dim=1)       
+        conv4 = self.dconv_down4(x)
+        x = self.maxpool(conv4)
+        x = self.dconv_down5(x)
 
+        x = self.upsample(x)    
+        x = torch.cat([x, conv4], dim=1)
+        x = self.dconv_up4(x)
+        x = self.upsample(x)  
+   
+        x = torch.cat([x, conv3], dim=1)
+        x = self.dconv_up3(x)
+        x = self.upsample(x) 
+    
+        x = torch.cat([x, conv2], dim=1)      
         x = self.dconv_up2(x)
-        x = self.upsample(x)        
+        x = self.upsample(x)    
         x = torch.cat([x, conv1], dim=1)   
-        
         x = self.dconv_up1(x)
         
         out = self.conv_last(x)
